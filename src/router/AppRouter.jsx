b71b186063e300d0db2cbbdefd042881543bd5a1
Robinson Betancur Marin <rbetancurm@ultragroupla.com>
2024-06-13 17:11:57 -0500

    add navbar and active class

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { LoginPage } from "../auth/pages/LoginPage";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { DcPage, MarvelPage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="marvel" element={<MarvelPage />} />
-      <Route path="dc" element={<DcPage />} />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='marvel' element={<MarvelPage />} />
+        <Route path='dc' element={<DcPage />} />
 
-      <Route path="login" element={<LoginPage />} />
+        <Route path='login' element={<LoginPage />} />
 
-      <Route path="/" element={<Navigate to="marvel" />} />
-    </Routes>
+        <Route path='/' element={<Navigate to='marvel' />} />
+      </Routes>
+    </>
   );
 };
